ac8f57e299697c7d1c5380510444beaee8f50b44
Gabriela Arrupes <gabrielarrupes@gmail.com>
2023-10-18 21:08:16 -0300

    migrations - typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,31 +1,3 @@
-import { DataSource, createConnection } from 'typeorm';
-//import { createConnection } from 'typeorm';
-//createConnection()
+import { createConnection } from 'typeorm';
 
-
-const LocalOrmconfig = require('../ormconfig.json')
-
-const dataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "gostack_gobarber",
-    migrations: [
-        "./src/database/migrations/*.ts",
-         
-    ],
-
-})
-
-dataSource.initialize()
-.then(() => {
-  console.log("Data Source has been initialized!");
-})
-.catch((err) => {
-  console.error("Error during Data Source initialization", err);
-});
-
-export default dataSource
-
+createConnection();
